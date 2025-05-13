12b97e67d59091c14081571138c2bd70d8b19b0a
KSPR <robot@kasparallenbach.ch>
2025-05-13 09:16:17 +0200

    syntax error

--- config/knock-knock.php
+++ config/knock-knock.php
@@ -1,12 +1,14 @@
 <?php
 
+use craft\helpers\App;
+
 return [
     '*' => [
         'enabled' => false,
         'enableCpProtection' => false,
-        'password' => App::env('STAGING_FRONTEND_PASSWORD')),
+        'password' => App::env('STAGING_FRONTEND_PASSWORD'),
         'siteSettings' => [],
-        'checkInvalidLogins' => false,
+        'checkInvalidLogins' => true,
         'invalidLoginWindowDuration' => '3600',
         'maxInvalidLogins' => 10,
     ],
