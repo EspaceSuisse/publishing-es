4b8e8f5ccaee1b470cec5601b9a863b24420df04
KSPR <robot@kasparallenbach.ch>
2025-05-27 17:15:09 +0200

    zeug

--- config/imager-x-transforms.php
+++ config/imager-x-transforms.php
@@ -4,8 +4,8 @@ return [
     'articleStarterImg' => [
         'displayName' => 'Article Aufmacherbild',
         'transforms' => [
-            ['width' => 1920, 'ratio' => 16 / 9],
-            ['width' => 1080, 'ratio' => 4 / 5],
+            ['width' => 1920, 'ratio' => 16 / 9, 'device' => 'desktop'],
+            ['width' => 1080, 'ratio' => 4 / 5, 'device' => 'mobile'],
         ],
         'defaults' => [
             'format' => 'avif'
@@ -14,18 +14,18 @@ return [
     'articleImage' => [
         'displayName' => 'Article Image',
         'transforms' => [
-            ['width' => 1920],
-            ['width' => 1080],
+            ['width' => 1920, 'device' => 'desktop'],
+            ['width' => 1080, 'device' => 'mobile'],
         ],
         'defaults' => [
-            'format' => 'avif'
+            'format' => 'avif',
         ],
     ],
     'articleThumb' => [
         'displayName' => 'Article Thumb',
         'transforms' => [
-            ['width' => 1920, 'ratio' => 1 / 1],
-            ['width' => 1080, 'ratio' => 1 / 1],
+            ['width' => 1920, 'ratio' => 1 / 1, 'device' => 'desktop'],
+            ['width' => 1080, 'ratio' => 1 / 1, 'device' => 'mobile'],
         ],
         'defaults' => [
             'format' => 'avif'
