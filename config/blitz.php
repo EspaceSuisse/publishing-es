4fa60c6b8d74cb98f1f1df721a755badae6a8243
KSPR <robot@kasparallenbach.ch>
2026-04-15 14:15:29 +0200

    ditch cloudflare

--- config/blitz.php
+++ config/blitz.php
@@ -4,22 +4,12 @@ use craft\helpers\App;
 
 return [
     '*' => [
-        'cachingEnabled' => false,
-        'refreshCacheAutomaticallyForGlobals' => false,
-        'debug' => true,
-        'queryStringCaching' => \putyourlightson\blitz\models\SettingsModel::QUERY_STRINGS_CACHE_URLS_AS_UNIQUE_PAGES,
         'includedUriPatterns' => [
             ['uriPattern' => '.*'],
         ],
-        'excludedUriPatterns' => [
-            ['uriPattern' => '^actions/knock-knock'],
-            ['uriPattern' => '^knock-knock'],
-            ['uriPattern' => '^mitglieder-login'],
-            ['uriPattern' => '^connexion-des-membres'],
-        ],
     ],
     'dev' => [
-        'cachingEnabled' => true,
+        'cachingEnabled' => false,
     ],
     'staging' => [
         'cachingEnabled' => false,
