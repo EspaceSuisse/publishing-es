2f5031af99c0571b6ff4f14356f3f998d2bd6020
KSPR <robot@kasparallenbach.ch>
2026-06-17 15:46:10 +0200

    add date maodified to filename pattern

--- config/imager-x.php
+++ config/imager-x.php
@@ -5,6 +5,8 @@ return [
         'bgColor' => '#ffffff',
         'cacheEnabled' => true,
         'cacheKeyPrefix' => 'imager-x',
+        'hashRemoteUrl' => true,
         'cacheDuration' => 2628000, // 1 month
+        'filenamePattern' => '{basename}_{transformString|shorthash}_{timestamp}.{extension}',
     ],
 ];
